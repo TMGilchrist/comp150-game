be9d19328436d6c7d0f89bb349e98689d7f24935
Louis <louis_foy@yahoo.co.uk>
2017-10-18 12:37:57 +0100

    - Updated game character graphic to be slightly more amazing - Added some extra comments - Added class skeletons: ObjectClass, CharacterClass, PlayerClass - Split rendering and image load into PlayerClass - Added timing code to main loop

--- player.py
+++ player.py
@@ -1,11 +1,57 @@
+# Standard imports
+import time
+
+# Third party imports
 import pygame
 
-class CharacterClass:
-    def character_movements(self):
+# Local imports (todo: remove these comments when we're all comfortable with
+#                this ordering)
 
 
+class ObjectClass:
+    # Main variables for characters
+    x = 0  # position is in game tiles
+    y = 0
+    sprite = None
+
+    def __init__(self, x, y):
+        """Initialise object at the given position"""
+        self.x = x
+        self.y = y
 
 
+    def move(self, move_x, move_y):
+        """Performs collision checking and moves object by offset of move_x and move_y if possible"""
+        self.x += move_x
+        self.y += move_y
+        # Todo: collision detection
+
+    def render(self):
+        """Renders the object sprite at its given position (overloadable function)"""
+        if self.sprite is not None:
+            screen.blit(self.sprite, (self.x, self.y))
+
+class CharacterClass(ObjectClass):
+    etc = 0  # Todo
+
+class PlayerClass(CharacterClass):
+    def __init__(self, x, y):
+        """Init: Loads default player sprite and scales it up"""
+        # Load character image
+        self.sprite = pygame.image.load('graphics/game_character.png')
+        self.sprite = self.sprite.convert(24)
+        # Scale character so we can see his beauty
+        self.sprite = pygame.transform.scale(
+                        self.sprite,
+                        (self.sprite.get_width() * 4, self.sprite.get_height() * 4))
+
+    def update(self):
+        # Character movement in response to input will go here :)
+
+
+# Constants
+TILE_WIDTH = 40  # temporary--plug in map defaults later!
+TILE_HEIGHT = 40  # ditto
 
 # Parameters
 g_width = 640
@@ -15,20 +61,29 @@ g_height = 480
 pygame.init()
 screen = pygame.display.set_mode((g_width, g_height))
 
-# Load character image
-character = pygame.image.load('graphics/game_character.png')
-character = character.convert(24)
+# Init character
+player = PlayerClass(0, 0)
 
-# Image scale
-character = pygame.transform.scale(character, (character.get_width() * 4, character.get_height() * 4))
+# Init main game parameters (todo: split all this into a class, no globals)
+tick_time = time.clock()  # time at the beginning of the current game tick
+delta_time = 0.0
 
 # Main loop
 running = True
 while running:
+    # Update timing
+    last_time = tick_time
+    tick_time = time.clock()
+
+    delta_time = tick_time - last_time
+
+    # Perform PyGame event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT or \
                 (event.type == pygame.KEYDOWN and
                  event.key == pygame.K_ESCAPE):
             running = False
-    screen.blit(character, (0, 0))
+
+    # Perform rendering (todo: move into separate Render class)
+    player.render()
     pygame.display.flip()
