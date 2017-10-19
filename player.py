374ab027e8533f70e052fbd18c5cc9942404c517
Tomas Mazurkevic <mazurkevic.tomas@gmail.com>
2017-10-19 15:15:35 +0100

    * Added pikachu objects * Added a list of pikachu objects * Rendered pikachu objects

--- player.py
+++ player.py
@@ -1,6 +1,7 @@
 # Standard imports
 import time
 import math
+import random
 
 # Third party imports
 import pygame
@@ -117,6 +118,15 @@ class PlayerClass(CharacterClass):
         self.move(self.x_velocity * delta_time, self.y_velocity * delta_time)
 
 
+class PikachuStatue(ObjectClass):
+    def __init__(self, x, y):
+        self.sprite = pygame.image.load('graphics/pikachu.png')
+        self.sprite = self.sprite.convert(24)
+        self.x = x
+        self.y = y
+
+
+
 # Constants
 TILE_WIDTH = 40  # temporary--plug in map defaults later!
 TILE_HEIGHT = 40  # ditto
@@ -132,6 +142,13 @@ screen = pygame.display.set_mode((g_width, g_height))
 # Init character
 player = PlayerClass(0, 0)
 
+
+# Init Objects
+pikachus = list()
+for i in xrange(10):
+    pikachus.append(PikachuStatue(random.randint(0, 10), random.randint(0, 10)))
+
+
 # Init main game parameters (todo: split all this into a class, no globals)
 tick_time = time.clock()  # time at the beginning of the current game tick
 delta_time = 0.0
@@ -145,6 +162,7 @@ while running:
 
     delta_time = tick_time - last_time
 
+
     # Perform PyGame event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT or \
@@ -155,5 +173,7 @@ while running:
     player.update()
 
     # Perform rendering (todo: move into separate Render class)
+    for pikachu in pikachus:
+        pikachu.render()
     player.render()
     pygame.display.flip()
