0b1a939dc471f6bd6604eebdb048efb80edf5656
Louis <louis_foy@yahoo.co.uk>
2017-10-30 11:29:06 +0000

    Refresher update--cleanup while waiting for Tomas - Polished for PEP-0008 compliance and fixed comments

--- FruitBats/HereBeDragons/Game.py
+++ FruitBats/HereBeDragons/Game.py
@@ -6,12 +6,15 @@ import pygame
 from Player import Player
 from TestObject import PikachuStatue
 
+
 class Game:
     delta_time = 0  # time passed since last frame
-    tick_time = 0  # time at the start of the frame, in seconds since the game started
-    screen = None  # pygame screen
+    tick_time = 0   # time at the start of the frame, in seconds since
+                    # the game started
+    start_time = 0  # initial time.clock() value on startup (OS-dependent)
+    screen = None   # PyGame screen
     objects = None  # list of active objects in the game
-    player = None  # pointer to the player object
+    player = None   # pointer to the player object
     quitting = False
     SCREEN_WIDTH = 640
     SCREEN_HEIGHT = 480
@@ -22,7 +25,8 @@ class Game:
     def run(self):
         # Init Python
         pygame.init()
-        self.screen = pygame.display.set_mode((self.SCREEN_WIDTH, self.SCREEN_HEIGHT))
+        self.screen = pygame.display.set_mode((self.SCREEN_WIDTH,
+                                               self.SCREEN_HEIGHT))
 
         # Init character
         self.player = Player(0, 0)
@@ -30,15 +34,15 @@ class Game:
         # Init Objects
         self.objects = list()
         for i in xrange(10):
-            self.objects.append(PikachuStatue(random.randint(0, 10), random.randint(0, 10)))
+            self.objects.append(PikachuStatue(random.randint(0, 10),
+                                              random.randint(0, 10)))
 
-        # Init main game parameters (todo: split all this into a class, no globals)
-        self.tick_time = time.clock()  # time at the beginning of the current game tick
+        # Init main game parameters
+        self.start_time = time.clock()
         self.delta_time = 0.0
 
         # Main loop
-        running = True
-        while running:
+        while not self.quitting:
             # Update timing
             last_time = self.tick_time
             self.tick_time = time.clock()
@@ -50,7 +54,7 @@ class Game:
                 if event.type == pygame.QUIT or \
                         (event.type == pygame.KEYDOWN and
                          event.key == pygame.K_ESCAPE):
-                    running = False
+                    self.quitting = True
 
             # Update player and objects
             self.player.update(self.delta_time, self.objects, None)
@@ -58,8 +62,8 @@ class Game:
             # Render (todo: move into separate Render class?)
             self.screen.fill((0, 0, 0))
 
-            for object in self.objects:
-                object.render(self.screen)
+            for obj in self.objects:
+                obj.render(self.screen)
             self.player.render(self.screen)
 
             # Splat to screen
